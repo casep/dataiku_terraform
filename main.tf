9f45cfdc9dd51bf667a1c525f11650b5b1293cf3
Carlos "casep" Sepulveda <carlos.sepulveda@gmail.com>
2025-07-23 16:04:48 +0100

    Initial version of deployment

---
+++ main.tf
@@ -0,0 +1,57 @@
+resource "aws_amplify_app" "dataiku_frontend" {
+  name         = var.app_name
+  repository   = var.github_repo
+  access_token = var.github_token
+
+  enable_auto_branch_creation = true
+
+  # The default patterns added by the Amplify Console.
+  auto_branch_creation_patterns = [
+    "*",
+    "*/**",
+  ]
+
+  auto_branch_creation_config {
+    # Enable auto build for the created branch.
+    enable_auto_build = true
+  }
+
+  # The default build_spec added by the Amplify Console for React.
+  build_spec = <<-EOT
+    version: 0.1
+    frontend:
+      phases:
+        preBuild:
+          commands:
+            - npm ci --cache .npm --prefer-offline
+        build:
+          commands:
+            - npm run build
+      artifacts:
+        baseDirectory: build
+        files:
+          - '**/*'
+      cache:
+        paths:
+          - node_modules/**/*
+  EOT
+
+  # The default rewrites and redirects added by the Amplify Console.
+  custom_rule {
+    source = "/<*>"
+    status = "404"
+    target = "/index.html"
+  }
+
+  environment_variables = {
+    ENV = "test"
+  }
+}
+
+resource "aws_amplify_branch" "master" {
+  app_id      = aws_amplify_app.dataiku_frontend.id
+  branch_name = "main"
+
+  stage = "PRODUCTION"
+
+}
